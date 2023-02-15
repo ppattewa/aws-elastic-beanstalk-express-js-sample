791c216b4571dabef5fa334d1b90a7ba5f5c5a21
Pankaj Pattewar <ppattewa@amazon.com>
2023-02-14 23:22:56 -0500

    Changing the connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -13,8 +13,16 @@ const mongoose = require('mongoose')
 //     useFindAndModify: false
 // })
 
-mongoose.connect('mongodb+srv://pankajp:Password12@cluster0.kayzu5x.mongodb.net/task-manager-api', {
+// mongoose.connect('mongodb+srv://pankajp:Password12@cluster0.kayzu5x.mongodb.net/task-manager-api', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// })
+
+mongoose.connect('mongodb://pankaj:Password12@docdb-demoapp.cluster-csbowdhwhfx2.us-east-1.docdb.amazonaws.com:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
+
+//mongodb://pankaj:Password12@docdb-demoapp.cluster-csbowdhwhfx2.us-east-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false
