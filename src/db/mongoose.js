b81901bae02dc3d41c6834db834d4f67ad5f8fd4
Pankaj Pattewar <ppattewa@amazon.com>
2023-02-15 16:58:05 -0500

    Playing around with connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -19,10 +19,11 @@ const mongoose = require('mongoose')
 //     useFindAndModify: false
 // })
 
-mongoose.connect('mongodb://pankaj:Password12@docdb-demoapp.cluster-csbowdhwhfx2.us-east-1.docdb.amazonaws.com:27017/task-manager-api', {
+mongoose.connect('mongodb://pankaj:Password12@docdb-demoapp.csbowdhwhfx2.us-east-1.docdb.amazonaws.com:27017/?retryWrites=false', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-})
+}).then(()=>console.log('Connection to db Successful'))
+.catch((err)=> console.error(err))
 
 //mongodb://pankaj:Password12@docdb-demoapp.cluster-csbowdhwhfx2.us-east-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false
